5aff25f9418882c4ab89e18d438c7e907540318d
WakeUpPig <1035465284qq.com>
2016-11-14 23:29:26 +0800

    reg

--- db/index.js
+++ db/index.js
@@ -1,7 +1,13 @@
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://106.14.31.43:27017/jwblog');
-
+mongoose.Promise = Promise;
 var UserSchema = new mongoose.Schema({
     username:String,
     password:String,
+
 });
+mongoose.model('User',UserSchema);
+
+global.Model = function (modName) {
+    return mongoose.model(modName);
+};
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.post('/reg', function(req, res, next) {
-  console.log(req.body);
-  res.end('1')
+  var user = req.body;
+  Model('User').create(user).then(function (data) {
+    res.send({err:0,msg:'注册成功'})
+  }).catch(function (err) {
+    res.send({err:1,msg:'注册失败'});
+  });
 });
 module.exports = router;
