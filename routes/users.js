c63ffc8935488c974d80d81de510fcad87c2e0cc
WakeUpPig <1035465284qq.com>
2016-11-15 00:35:56 +0800

    reg

--- routes/users.js
+++ routes/users.js
@@ -5,12 +5,10 @@ var router = express.Router();
 /* GET users listing. */
 router.post('/reg', function(req, res, next) {
   var user = req.body;
-  console.log(user.password);
   user.password = crypto.createHash('md5').update(user.password.toString()).digest('hex');
   Model('User').create(user).then(function (data) {
-    delete user.password;
     req.session.user = user;
-    res.send({err:0,msg:'注册成功',user:req.session.user})
+    res.send({err:0,msg:'注册成功',username:data.username,id:data._id})
   }).catch(function (err) {
     res.send({err:1,msg:'注册失败'});
   });
