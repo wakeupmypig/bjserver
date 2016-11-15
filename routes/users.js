cbdc0edb80d95ebf173d05901cd87ff05f8a9cdb
WakeUpPig <1035465284qq.com>
2016-11-15 23:18:46 +0800

    login

--- routes/users.js
+++ routes/users.js
@@ -2,15 +2,35 @@ var express = require('express');
 var crypto = require('crypto');
 var router = express.Router();
 
-/* GET users listing. */
+/*注册接口*/
 router.post('/reg', function(req, res, next) {
   var user = req.body;
+  if(user.username!=undefined) {
+    return res.send({err:1,msg:'用户不能为空'});
+  }
+  if(user.password!=user.repassword) {
+    return res.send({err:1,msg:'两次密码不一致'});
+  }
+  if(user.username<=6&&user.password<=6){
+    return res.send({err:1,msg:'用户名或密码长度过短'});
+  }
+    user.password = crypto.createHash('md5').update(user.password.toString()).digest('hex');
+    Model('User').create(user).then(function (data) {
+      req.session.user = user;
+      res.send({err:0,msg:'注册成功',username:data.username,id:data._id})
+    }).catch(function (err) {
+      res.send({err:1,msg:'注册失败'});
+    });
+});
+/*登录*/
+router.post('/login', function(req, res, next) {
+  var user = req.body;
   user.password = crypto.createHash('md5').update(user.password.toString()).digest('hex');
-  Model('User').create(user).then(function (data) {
-    req.session.user = user;
-    res.send({err:0,msg:'注册成功',username:data.username,id:data._id})
+  Model('User').findOne(user).then(function (data) {
+      req.session.user = user;
+      res.send({err:0,msg:'登录成功',username:data.username,id:data._id})
   }).catch(function (err) {
-    res.send({err:1,msg:'注册失败'});
+      res.send({err:1,msg:'登录失败'});
   });
 });
 module.exports = router;
